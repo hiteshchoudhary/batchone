715d56e623a7ec26b38b9f96ebbfa51a4ffa0c2a
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2022-12-04 06:03:22 +0000

    connected to DB with iife

---
+++ app.js
@@ -0,0 +1,6 @@
+import express from 'express'
+
+const app = express()
+
+
+export default app
--- config/index.js
+++ config/index.js
@@ -5,6 +5,8 @@ dotenv.config()
 const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+    MONGODB_URL: process.env.MONGODB_URL,
+    PORT: process.env.PORT
 }
 
 export default config
