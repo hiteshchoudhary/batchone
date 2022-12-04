e4bc66c1520c91aa06fa61a4b236b3e800564187
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2022-12-04 07:10:05 +0000

    created a mail utility

--- config/index.js
+++ config/index.js
@@ -6,7 +6,12 @@ const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
     MONGODB_URL: process.env.MONGODB_URL,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    SMTP_MAIL_HOST: process.env.SMTP_MAIL_HOST,
+    SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT,
+    SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
+    SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
+    SMTP_MAIL_EMAIL: process.env.SMTP_MAIL_EMAIL
 }
 
 export default config
