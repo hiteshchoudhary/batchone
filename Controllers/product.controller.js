0a988e59b52051f9042701018e6c251ebd5ff0c0
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-02-27 18:21:56 +0530

    adding functionality to remove img from s3 bucket if  product does not get created

--- Controllers/product.controller.js
+++ Controllers/product.controller.js
@@ -15,8 +15,6 @@ import config from "../config/index.js"
  * @descriptio Uses AWS S3 Bucket for image upload
  * @returns Product Object
  *********************************************************/
-
-
 export const addProduct = asyncHandler(async (req, res) => {
     const form = formidable({
         multiples: true,
@@ -68,8 +66,16 @@ export const addProduct = asyncHandler(async (req, res) => {
             })
 
             if (!product) {
-                throw new CustomError("Product was not created", 400)
                 //remove image
+                let deleteImgArray = Promise.all(
+                    Object.keys(files).map(async (filekey, index) => {
+                        await deleteFile({
+                            bucketName: config.S3_BUCKET_NAME,
+                            key: `products/${productId}/photo_${index + 1}.png`
+                        })
+                    })
+                )
+                throw new CustomError("Product was not created", 400)
             }
             res.status(200).json({
                 success: true,
@@ -83,7 +89,7 @@ export const addProduct = asyncHandler(async (req, res) => {
             })
         }
     })
-})
+}) 
 
 /**********************************************************
  * @GET_ALL_PRODUCT
@@ -92,7 +98,6 @@ export const addProduct = asyncHandler(async (req, res) => {
  * @description User and admin can get all the prducts
  * @returns Products Object
  *********************************************************/
-
 export const getAllProducts = asyncHandler( async (req, res) => {
     const products = await Product.find({})
 
@@ -113,8 +118,6 @@ export const getAllProducts = asyncHandler( async (req, res) => {
  * @description User and admin can get single product details
  * @returns Product Object
  *********************************************************/
-
-
 export const getProductById = asyncHandler( async (req, res) => {
     const {id: productId} = req.params
 
